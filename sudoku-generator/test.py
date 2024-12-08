d5ce8a3206f6e7e6b35004b68b784c352036e639
Akkaro <akos.beno@gmail.com>
2024-12-08 19:12:10 +0200

    deleted unnecessary files

--- sudoku-generator/test.py
+++
@@ -1,8 +0,0 @@
-from generator.mace4_interface import run_mace4
-from backend.sudoku_logic import get_sudoku_from_mace4, extract_and_save_sudoku_grid
-def main():
-    get_sudoku_from_mace4()
-    extract_and_save_sudoku_grid()
-
-if __name__ == "__main__":
-    main()
